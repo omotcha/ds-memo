341ffdb9ced623b442543ff5a547b1be15ae0a57
omotcha <omotcha2097@gmail.com>
2023-07-04 13:58:26 +0800

    contract + embeddings util init

---
+++ configs/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# --*-- coding: utf-8 --*--
+# @Author: Liu Tengfei
+# @Email: liutengfei@chinadep.com
+# @Time: 2023/7/4 13:26
+# @File: __init__.py.py
+# @Description:
---
+++ embeddings/embeddings_util.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# --*-- coding: utf-8 --*--
+# @Author: Liu Tengfei
+# @Email: liutengfei@chinadep.com
+# @Time: 2023/7/4 11:53
+# @File: embeddings_util.py
+# @Description:
